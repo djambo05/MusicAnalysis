9a506b9e75257f727e3bff35e8450ea33db16c7c
Djamalutin <fisher.gerryy@mail.ru>
2023-05-05 16:18:03 +0300

    feature: space-between

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -7,62 +7,84 @@ const SideBar = ({ sections, status }) => {
         height: "100%",
         backgroundColor: "#0E284F",
         position: "fixed",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
         top: 0,
         left: 0,
       }}
     >
-      <div
-        style={{
-          width: "100%",
-          height: "80px",
-          backgroundColor: "#3051FF",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <img src="./img/music-icon.png" alt="music-icon"></img>
-      </div>
+      <aside>
+        <div
+          style={{
+            width: "100%",
+            height: "80px",
+            backgroundColor: "#3051FF",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <img src="./img/music-icon.png" alt="music-icon"></img>
+        </div>
+        <ul
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+            height: "290px",
+            listStyle: "none",
+            marginTop: "31px",
+            marginBottom: 0,
+            padding: "0px 0px",
+          }}
+        >
+          {sections.map((section) => (
+            <li
+              key={section.id}
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "40px",
+                width: "100%",
+              }}
+              onClick={() => {
+                status(section);
+              }}
+              onMouseOver={(e) => {
+                e.target.style.background =
+                  "linear-gradient(90deg,  rgba(255, 255, 255, 0.15) -7.41%, rgba(255, 255, 255, 0) 38.5%)";
+              }}
+              onMouseOut={(e) => {
+                e.target.style.background = "transparent";
+              }}
+            >
+              <img
+                style={{ pointerEvents: "none" }}
+                src={`./img/${section.img}`}
+                alt={`${section.alt}`}
+              ></img>
+            </li>
+          ))}
+        </ul>
+      </aside>
       <ul
         style={{
           display: "flex",
           flexDirection: "column",
           justifyContent: "space-between",
-          height: "290px",
           listStyle: "none",
-          marginTop: "31px",
-          marginBottom: 0,
+          marginTop: 0,
           padding: "0px 0px",
         }}
       >
-        {sections.map((section) => (
-          <li
-            key={section.id}
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              height: "40px",
-              width: "100%",
-            }}
-            onClick={() => {
-              status(section);
-            }}
-            onMouseOver={(e) => {
-              e.target.style.background =
-                "linear-gradient(90deg,  rgba(255, 255, 255, 0.15) -7.41%, rgba(255, 255, 255, 0) 38.5%)";
-            }}
-            onMouseOut={(e) => {
-              e.target.style.background = "transparent";
-            }}
-          >
-            <img
-              style={{ pointerEvents: "none" }}
-              src={`./img/${section.img}`}
-              alt={`${section.alt}`}
-            ></img>
-          </li>
-        ))}
+        <li>
+          <img alt=""></img>
+        </li>
+        <li>
+          <img src="./img/avatar.png" alt="avatar"></img>
+        </li>
       </ul>
     </nav>
   );
